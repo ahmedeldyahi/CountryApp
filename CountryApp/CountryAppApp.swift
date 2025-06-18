7b642f117a950a459073221ac4f9dc9519e2eda0
Ahmed Eldyahi <ahmed.eldyahi@mdi.com.eg>
2025-06-18 18:00:47 +0300

    Initial Commit

---
+++ CountryApp/CountryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryAppApp.swift
+//  CountryApp
+//
+//  Created by Ahmed Eldyahi on 18/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CountryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
